e2ae726f3a35d409fc47191502225fef3cb3670c
skyxuan <syncshinee@gmail.com>
2016-04-29 17:41:01 +0800

    KTU Programming Camp(Day 2) E F, #1

--- KTUProgrammingCampDay2/E.cpp
+++ KTUProgrammingCampDay2/E.cpp
@@ -7,12 +7,11 @@ using namespace std;
 const int N = 200010;
 struct node {
     int x, y, val;
-    node (int _x = 0, _y = 0, _v = 0) : x(_x), y(_y), val(_v) {}
+    node (int _x = 0, int _y = 0, int _v = 0) : x(_x), y(_y), val(_v) {}
 }e[N << 1];
-operator < (const node &A, const int &B) {
+bool operator < (const node &A, const node &B) {
     return A.val < B.val;
 }
-vector <string> ans;
 int f[N], Rank[N];
 int gf(int x) {
     return x == f[x] ? x : f[x] = gf(f[x]);
@@ -20,7 +19,56 @@ int gf(int x) {
 int merge(int x, int y) {
     int i = gf(x), j = gf(y);
     if (Rank[i] < Rank[j]) {
-	
+	f[i] = j;
+	f[x] = j;
+    }
+    else {
+	f[j] = i;
+	f[y] = i;
+	if (Rank[i] == Rank[j]) {
+	    Rank[i] ++;
+	}
+    }
+}
+bool judge(int x, int y) {
+    return gf(x) == gf(y);
+}
+vector <int> G[N];
+vector <int> driver[N];
+int bus[N];
+int deep[N];
+struct comp {
+    int x, id;
+    comp(int _x = 0, int _i = 0) : x(_x), id(_i) {}
+};
+bool operator < (const comp &A, const comp &B) {
+    return A.x < B.x;
+}
+void dfs(int x, int pre) {
+    comp mx = comp(0, x);
+    for (int i = 0; i < G[x].size(); ++ i) {
+	if (G[x][i] == pre) continue;
+	dfs(G[x][i], x);
+	mx = max(mx, comp(deep[G[x][i]], G[x][i]));
+	printf("Drive %d %d %d\n", bus[G[x][i]], G[x][i], x);
+    }
+    if (mx.id != x) {
+	bus[x] = bus[mx.id];
+	printf("Move %d %d %d\n", x, x, bus[x]);
+    }
+    deep[x] = mx.x;
+    for (int i = 0; i < G[x].size(); ++ i) {
+	if (G[x][i] == pre) continue;
+	int v = G[x][i];
+	if (bus[v] != bus[x] && deep[v] == deep[x]) {
+	    deep[x] ++;
+	}
+	for (int j = 0; j < driver[v].size(); ++ j) {
+	    driver[x].push_back(driver[v][j]);
+	    if (bus[v] != bus[x]) {
+		printf("Move %d %d %d\n", driver[v][j], bus[v], bus[x]);
+	    }
+	}
     }
 }
 int main() {
@@ -29,11 +77,28 @@ int main() {
     for (int i = 1; i <= n; ++ i) {
 	f[i] = i;
 	Rank[i] = 1;
+	driver[i].push_back(i);
+	bus[i] = i;
     }
     for (int i = 0; i < m; ++ i) {
 	scanf("%d%d%d", &e[i].x, &e[i].y, &e[i].val);
     }
     sort(e, e + m);
-    
+    int ans = 0, num = 0;
+    for (int i = 0; i < m; ++ i) {
+	if (!judge(e[i].x, e[i].y)) {
+	    merge(e[i].x, e[i].y);
+	    G[e[i].x].push_back(e[i].y);
+	    G[e[i].y].push_back(e[i].x);
+	    ans += e[i].val;
+	    num ++;
+	    if (num == n - 1) {
+		break;
+	    }
+	}
+    }
+    printf("%d\n", ans);
+    dfs(1, -1);
+    puts("Done");
     return 0;
 }
--- KTUProgrammingCampDay2/F.cpp
+++ KTUProgrammingCampDay2/F.cpp
@@ -1,1 +1,106 @@
-#
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <map>
+#define lson l, mid, rt << 1
+#define rson mid + 1, r, rt << 1 | 1
+using namespace std;
+const int N = 100010;
+int Q;
+struct node {
+    int len, k;
+    node(int _l = 0, int _k = 0) : len(_l), k(_k) {}
+}q[N];
+bool operator <(const node &A, const node &B) {
+    return A.len > B.len;
+}
+int sa[N], t1[N], t2[N], c[N];
+int Rank[N];
+void buildsa(char *s, int n, int m) {
+    int i, k, p, a1, a2, *x = t1, *y = t2;
+    memset(c, 0, m << 2);
+    for (i = 0; i < n; ++ i) ++ c[x[i] = s[i]];
+    for (i = 1; i < m; ++ i) c[i] += c[i - 1];
+    for (i = n - 1; i >= 0; -- i) sa[--c[x[i]]] = i;
+    for (k = 1, p = 0; k < n; k << 1, p = 0) {
+	for (i = n - k; i < n; ++ i) y[p ++] = i;
+	for (i = 0; i < n; ++ i) if (sa[i] >= k) y[p ++] = sa[i] - k;
+	memset(c, 0, m << 2);
+	for (i = 0; i < n; ++ i) ++ c[x[y[i]]];
+	for (i = 1; i < m; ++ i) c[i] += c[i - 1];
+	for (i = n - 1; i >= 0; -- i) sa[-- c[x[y[i]]]] = y[i];
+	swap(x, y), p = 1, x[sa[0]] = 0;
+	for (i = 1; i < n; ++ i) {
+	    a1 = sa[i - 1] + k < n ? y[sa[i - 1] + k] : -1;
+	    a2 = sa[i] + k < n ? y[sa[i] + k] : -1;
+	    x[sa[i]] = (y[sa[i - 1]] == y[sa[i]] && a1 == a2) ? p - 1 : p ++;
+	}
+	if (p >= n) break;
+	m = p;
+    }
+    for (i = 0; i < n; ++ i) Rank[sa[i]] = i;
+}
+
+char s[N];
+
+struct TreeNode {
+    int sum, ri;
+    TreeNode (int _s = 0, int _r = -1) : sum(_s), ri(_r) {}
+}T[N << 2];
+
+void pushup(int rt) {
+    T[rt].sum = T[rt << 1].sum + T[rt << 1 | 1].sum;
+    T[rt].ri = T[rt << 1 | 1].ri == -1 ? T[rt << 1].ri : T[rt << 1 | 1].ri;
+}
+
+void update(int x, int val, int l, int r, int rt) {
+    if (l == r && l == x) {
+	T[rt].sum = 1;
+	T[rt].ri = val;
+	return;
+    }
+    int mid = (l + r) >> 1;
+    if (x <= mid) {
+	update(x, val, lson);
+    }
+    else {
+	update(x, val, rson);
+    }
+    pushup(rt);
+}
+
+int getk(int k, int l, int r, int rt) {
+    if (T[rt].sum == k) {
+	return T[rt].ri;
+    }
+    int tmp = T[rt << 1].sum;
+    int mid = (l + r) >> 1;
+    if (tmp < k) {
+	return getk(k - tmp, rson);
+    }
+    return getk(k, lson);
+}
+
+int main() {
+    scanf("%s", s);
+    int len = strlen(s);
+    scanf("%d", &Q);
+    for (int i = 0; i < Q; ++ i) {
+	scanf("%d%d", &q[i].len, &q[i].k);
+    }
+    sort(q, q + Q);
+    buildsa(s, len, 128);
+    int nw = len + 1;
+    for (int i = 0; i < Q; ++ i) {
+	while (nw > 1 && nw > q[i].len) {
+	    nw --;
+	    update(Rank[len - nw], len - nw + 1, 0, len - 1, 1);
+	}
+	printf("%d\n", getk(q[i].k, 0, len - 1, 1));
+    }
+    return 0;
+}
+
